26eda648839c4c6da878f8eb398eccceccf61cd2
yekyunglee <skybluee2014@gmail.com>
2020-10-20 18:33:51 +0900

    add mousemove js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const canvas = document.getElementById("jsCanvas");
+
+
+//특정 이벤트가 발생했을 시 특정 함수를 실행할 수 있게 해주는 addEventListener
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY
+
+}
+
+if (canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+}
