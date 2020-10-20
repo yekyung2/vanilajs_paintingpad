6d4e5169f2288d98d5f973cb2d42756bbef3a9e6
yekyunglee <skybluee2014@gmail.com>
2020-10-20 18:56:30 +0900

    add mouseleave, stopPainting function

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const canvas = document.getElementById("jsCanvas");
 let painting = false; 
+function stopPainting(){
+    painting = false;
+}
 
-//특정 이벤트가 발생했을 시 특정 함수를 실행할 수 있게 해주는 addEventListener
+
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
@@ -12,12 +15,16 @@ function onMouseDown(event){
 }
 
 function onMouseUp(event){
-    painting = false;
+    stopPainting();
+    
 }
 
 
+//특정 이벤트가 발생했을 시 특정 함수를 실행할 수 있게 해주는 addEventListener 
+// 적용할변수.addEventListner(addeventListner가 가지고 있는 이벤트 이름, 내가 지정한 함수이름)
 if (canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", onMouseDown);
     canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", stopPainting);
 }
